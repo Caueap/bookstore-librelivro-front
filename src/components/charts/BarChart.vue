4b79e5fa377f9071523f0ef56a19723279860d2c
Cauê Proença <caueabproenca@gmail.com>
2023-02-14 15:40:23 -0300

    improved responsivity on items components (not on chart)

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <barchart type="bar" width="500" :options="options" :series="series" class="chart"></barchart>
+    </div>
+</template>
+
+<script>
+import barchart from 'vue-apexcharts';
+
+export default {
+    name: 'BarChart',
+    data() {
+        return {
+            options: {
+                chart: {
+                    id: 'barchart',
+                },
+                xaxis: { 
+                    categories: [
+                        'O Velho e o Mar',
+                        'A Metamorfose',
+                        'Harry Potter e a Pedra Filosofal',
+                        'O Lobo da Estepe',
+                        'Berserk',
+                    ],
+                    labels: {
+                        show: false,
+                        // trim: true,
+                        // rotate: 0
+                    },
+                },
+
+                // responsive: [
+                //     {
+                //         breakpoint: 700,
+                //         options: {
+                //             series: [30, 40, 45, 50, 60],
+                //             chart: {
+                //                 type: 'pie',
+                //                 width: 200,
+                //             },
+                //         },
+                        // labels: ['']
+                        // options: {
+                        //     chart: {
+                        //         width: 250,
+                        //         // height: '80%'
+                        //     },
+                        //     plotOptions: {
+                        //         bar: {
+                        //             horizontal: true
+                        //         }
+                        //     }
+                        // },
+                    
+                
+
+                colors: ['#00695c'],
+            },
+            series: [
+                {
+                    name: 'series-1',
+                    data: [30, 40, 45, 50, 60],
+                },
+            ],
+        };
+    },
+    components: {
+        barchart,
+    },
+}
+
+</script>
+    
+<style scoped>
+
+/* @media (max-width: 400px) {
+    .chart {
+        width: 50%;
+    }
+} */
+
+
+
+</style>
--- src/components/home/HomeComponent.vue
+++ src/components/home/HomeComponent.vue
@@ -1,35 +1,31 @@
 <template class="t">
     <div class="maindiv">
-        <div class="item-group-div">
-            <v-item-group mandatory>
-                <v-container>
-                    <v-row justify="space-around" class="space mainrow">
-                        <v-col cols="12" md="2">
-                            <v-item></v-item>
-                            <v-card class="d-flex align-center rounded-xl" color="#004D40" height="200">
-                                <v-row>
-                                    <v-col cols="12" sm="12">
-                                        <v-list-item three-line class="mt-10">
-                                            <v-list-item-content>
-                                                <div class="mb-4">
-                                                    <v-icon x-large color="white">mdi mdi-account</v-icon>
-                                                </div>
-                                                <v-list-item-subtitle class="subtitle-color">
-                                                    Usuários
-                                                </v-list-item-subtitle>
-                                                <v-list-item-title class="headline mb-1 title-color" color="#fff"
-                                                    >52
-                                                </v-list-item-title>
-                                                <strong></strong>
-                                            </v-list-item-content>
-                                        </v-list-item>
-                                    </v-col>
-                                </v-row>
+        <div class="content-div">
+            
+            <v-container fluid>
+                <v-item-group>
+                <v-row justify="space-around" class="space mainrow">
+                   
+                    <v-col cols="12" md="2" xl="2" sm="6" class="col">
+                        <v-item>
+                            <v-card class="d-flex align-center rounded-xl card" color="#004D40" height="200">
+                                <v-list-item three-line class="mt-10">
+                                    <v-list-item-content>
+                                        <div class="mb-4">
+                                            <v-icon x-large color="white">mdi mdi-account</v-icon>
+                                        </div>
+                                        <v-list-item-subtitle> Usuários </v-list-item-subtitle>
+                                        <v-list-item-title class="mb-1">52 </v-list-item-title>
+                                        <strong></strong>
+                                    </v-list-item-content>
+                                </v-list-item>
                             </v-card>
-                        </v-col>
-                        <v-col cols="12" md="2">
-                            <v-item></v-item>
-                            <v-card class="d-flex align-center rounded-xl" color="#004D40" height="200">
+                        </v-item>
+                    </v-col>
+
+                    <v-col cols="12" md="2" xl="2" sm="6" class="col">
+                        <v-item>
+                            <v-card class="d-flex align-center rounded-xl card" color="#004D40" height="200">
                                 <v-row>
                                     <v-col cols="12" sm="12">
                                         <v-list-item three-line class="mt-10">
@@ -37,22 +33,20 @@
                                                 <div class="mb-4">
                                                     <v-icon x-large color="white">mdi mdi-domain</v-icon>
                                                 </div>
-                                                <v-list-item-subtitle class="subtitle-color">
-                                                    Editoras
-                                                </v-list-item-subtitle>
-                                                <v-list-item-title class="headline mb-1 title-color" color="#fff"
-                                                    >20
-                                                </v-list-item-title>
+                                                <v-list-item-subtitle> Editoras </v-list-item-subtitle>
+                                                <v-list-item-title class="mb-1" color="#fff">20 </v-list-item-title>
                                                 <strong></strong>
                                             </v-list-item-content>
                                         </v-list-item>
                                     </v-col>
                                 </v-row>
                             </v-card>
-                        </v-col>
-                        <v-col cols="12" md="2">
-                            <v-item></v-item>
-                            <v-card class="d-flex align-center rounded-xl" color="#004D40" height="200">
+                        </v-item>
+                    </v-col>
+                
+                    <v-col cols="12" md="2" xl="2" sm="6" class="col">
+                        <v-item>
+                            <v-card class="d-flex align-center rounded-xl card" color="#004D40" height="200">
                                 <v-row>
                                     <v-col cols="12" sm="12">
                                         <v-list-item three-line class="mt-10">
@@ -60,22 +54,20 @@
                                                 <div class="mb-4">
                                                     <v-icon x-large color="white">mdi-book-open-blank-variant</v-icon>
                                                 </div>
-                                                <v-list-item-subtitle class="subtitle-color">
-                                                    Livros
-                                                </v-list-item-subtitle>
-                                                <v-list-item-title class="headline mb-1 title-color" color="#fff"
-                                                    >129
-                                                </v-list-item-title>
+                                                <v-list-item-subtitle> Livros </v-list-item-subtitle>
+                                                <v-list-item-title class="mb-1" color="#fff">129 </v-list-item-title>
                                                 <strong></strong>
                                             </v-list-item-content>
                                         </v-list-item>
                                     </v-col>
                                 </v-row>
                             </v-card>
-                        </v-col>
-                        <v-col cols="12" md="2">
-                            <v-item></v-item>
-                            <v-card class="d-flex align-center rounded-xl" color="#004D40" height="200">
+                        </v-item>
+                    </v-col>
+                    
+                    <v-col cols="12" md="2" xl="2" sm="6" class="col">
+                        <v-item>
+                            <v-card class="d-flex align-center rounded-xl card" color="#004D40" height="200">
                                 <v-row>
                                     <v-col cols="12" sm="12">
                                         <v-list-item three-line class="mt-10">
@@ -85,58 +77,38 @@
                                                         >mdi-badge-account-horizontal-outline</v-icon
                                                     >
                                                 </div>
-                                                <v-list-item-subtitle class="subtitle-color">
-                                                    Aluguéis
-                                                </v-list-item-subtitle>
-                                                <v-list-item-title class="headline mb-1 title-color" color="#fff"
-                                                    >35
-                                                </v-list-item-title>
+                                                <v-list-item-subtitle> Aluguéis </v-list-item-subtitle>
+                                                <v-list-item-title class="mb-1" color="#fff">35 </v-list-item-title>
                                                 <strong></strong>
                                             </v-list-item-content>
                                         </v-list-item>
                                     </v-col>
                                 </v-row>
                             </v-card>
-                        </v-col>
-                    </v-row>
-                </v-container>
+                        </v-item>
+                    </v-col>
+               
+                </v-row>
             </v-item-group>
+            </v-container>
+        
 
-            <div class="colchart">
-                <colchart type="bar" width="500" :options="options" :series="series"></colchart>
-            </div>
+            <v-row>
+                <v-col cols="12" md="6" lg="6" xl="6" xs="12">
+                    <BarChart />
+                </v-col>
+            </v-row>
         </div>
     </div>
 </template>
 
 <script>
-import colchart from 'vue-apexcharts';
+import BarChart from '@/components/charts/BarChart.vue';
 
 export default {
     name: 'HomeComponent',
-    data() {
-        return {
-            options: {
-                chart: {
-                    id: 'vuechart-example',
-                },
-                xaxis: {
-                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-                },
-                colors: ['#00695c']
-            },
-            series: [
-                {
-                    name: 'series-1',
-                    data: [30, 40, 45, 50, 49, 60, 70, 91],  
-                },
-
-            ],
-            
-        };
-    },
     components: {
-        colchart,
+        BarChart,
     },
 };
 </script>
@@ -147,26 +119,65 @@ export default {
     height: 100%;
 }
 
-.item-group-div {
+.content-div {
     margin: 25px 50px 50px 50px;
     background-color: #fff;
-    height: 92%;
+    height: 94%;
 }
 
 .mainrow {
     justify-content: space-between;
+    
 }
 
-.title-color {
+.v-card{
+    width: 220px;
+}
+
+.v-list-item__subtitle {
+    font-size: 28px;
+    
+}
+
+/*Esse seletor vem por padrão no v-list-item__subtitle, e seta a cor do subtitle pra preta. Aqui, o seletor padrão
+é sobrescrevido e a cor é setada pra branca. Isso tem que ser feito pois esse seletor é padrão do componente, colocar
+uma classe no componente e colocar a cor branca não funciona por que esse seletor tem prioridade*/
+.theme--light.v-list-item:not(.v-list-item--disabled) .v-list-item__subtitle, .theme--light.v-list-item:not(.v-list-item--disabled) .v-list-item__action-text {
+    color: #fff
+}
+
+.v-list-item__title {
+    font-size: 32px;
     color: #fff;
 }
 
-.theme--light.v-list-item:not(.v-list-item--disabled) .v-list-item__subtitle,
+
+
+/* .itemtitle {
+    color: #fff;
+    font-size: 32px;
+} */
+
+/* .theme--light.v-list-item:not(.v-list-item--disabled) .v-list-item__subtitle,
 .theme--light.v-list-item:not(.v-list-item--disabled) .v-list-item__action-text {
     color: #fff;
-}
+} */
 
-.colchart {
-    margin: 50px 0px 20px 75px
-}
+/* .subtitle {
+    font-size: 26px;
+} */
+
+/* .card {
+        width: fit-content;
+    } */
+
+/* .colchart {
+    margin: 50px 0px 20px 75px;
+}*/
+
+/* @media (max-width: 380px) {
+    .colchart {
+        margin: auto;
+    }
+}  */
 </style>
